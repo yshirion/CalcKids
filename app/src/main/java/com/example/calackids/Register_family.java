30dc97f165f884354ff33f35594e681e00858210
Yechiel Shirion <yzshirion@gmail.com>
2022-08-01 23:01:23 +0300

    init commit

---
+++ app/src/main/java/com/example/calackids/Register_family.java
@@ -0,0 +1,14 @@
+package com.example.calackids;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register_family extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_family);
+    }
+}
